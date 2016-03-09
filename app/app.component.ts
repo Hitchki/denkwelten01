49fcc25e36779259fc4a4250a7f087d621bbc114
Franz Irnberger <franz.irnberger@gmail.com>
2016-03-09 10:23:21 +0100

    5 minutes quickstart with app/js folder

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>My second Angular2 Application</h1>
+        <h2>directory js for js-Files and templates</h2>
+        `
+})
+export class AppComponent{  };
+
+
+
+
