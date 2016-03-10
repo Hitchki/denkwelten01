91acb5dfec2037e65aef1a30f6568c3a6e21566a
webstorm denkwelten01-webstorm <franz.irnberger@gmail.com>
2016-03-10 06:58:18 +0100

    added welcome.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,24 @@
 import {Component} from 'angular2/core';
 import {OnInit} from 'angular2/core';
 
+import {WelcomeComponent} from './common/welcome.component';
+
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
     template: `
-        <h1>My second Angular2 Application</h1>
-        <h2>directory js for js-Files and templates</h2>
-        `
+        <h1>Denkwelten</h1>
+        <h2>{{title}}directory js for js-Files and templates</h2>
+        <welcome-component></welcome-component>
+        `,
+    directives : [WelcomeComponent]
 })
-export class AppComponent{  };
-
-
-
+export class AppComponent{
+    public title = 'Denkwelten';
+    public hero = 'Franz';
+};
 
