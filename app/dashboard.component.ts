84ade567a80c18415af6826cb42a3a47fbc8330b
webstorm denkwelten01-webstorm <franz.irnberger@gmail.com>
2016-03-11 10:36:01 +0100

    Ang2-TUT5 my end-version

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
 
 import { Hero } from './hero/hero';
 import { HeroService } from './hero.service';
@@ -6,14 +7,22 @@ import { HeroService } from './hero.service';
 @Component({
     selector: 'my-dashboard',
     templateUrl: 'app/dashboard.component.html',
+    styleUrls: ['app/dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
     heroes: Hero[] = [];
-    constructor(private _heroService: HeroService) { }
+    constructor(
+        private _router: Router,
+        private _heroService: HeroService) {
+    }
+
     ngOnInit() {
         this._heroService.getHeroes()
             .then(heroes => this.heroes = heroes.slice(1,5));
     }
-    gotoDetail(){ /* not implemented yet */}
+    gotoDetail(hero: Hero) {
+        let link = ['HeroDetail', { id: hero.id }];
+        this._router.navigate(link);
+    }
 }
 
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,25 +1,14 @@
 import { Component, OnInit } from 'angular2/core';
 import {RouteParams} from 'angular2/router';
 
-import {Hero} from './hero/hero.ts';
+import { Hero } from './hero/hero';
 import { HeroService } from './hero.service';
-
 
 @Component({
     selector: 'my-hero-detail',
-    template: `
-        <div *ngIf="hero">
-            <h2>{{hero.name}} details!</h2>
-            <div><label>id: </label>{{hero.id}}</div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="hero.name" placeholder="name"/>
-            </div>
-        </div>
-        `,
-    inputs: ['hero']
+    templateUrl: 'app/hero-detail.component.html',
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
     hero: Hero;
 
     constructor(
@@ -33,4 +22,7 @@ export class HeroDetailComponent {
             .then(hero => this.hero = hero);
     }
 
+    goBack() {
+        window.history.back();
+    }
 }
